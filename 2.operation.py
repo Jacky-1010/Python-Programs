b3d506b56498502e412a38ca337c1971049cdbc9
Jacky-1010 <priyanshumishra2143@gmail.com>
2026-08-23 02:09:34 +0530

    Delete 2.operation.py

--- 2.operation.py
+++
@@ -1,6 +0,0 @@
-num1=int(input("Enter first no.:"))
-num2=int(input("Enter second no.:"))
-print(num1,"+",num2,"=",num1+num2)
-print(num1,"-",num2,"=",num1-num2)
-print(num1,"*",num2,"=",num1*num2)
-print(num1,"/",num2,"=",num1/num2)
