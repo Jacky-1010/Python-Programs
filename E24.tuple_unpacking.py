ec7c157e4a09826c3b05f364cd0dbda01e12d45e
Jacky-1010 <priyanshumishra2143@gmail.com>
2026-09-01 00:41:38 +0530

    Add files via upload

---
+++ E24.tuple_unpacking.py
@@ -0,0 +1,10 @@
+#tuple creation
+stud=("Alex",18,"FYIT",6111)
+print(stud)
+#tuple unpacking
+name,age,batch,roll=stud
+print("Student details")
+print("Name:",name)
+print("Age:",age)
+print("Batch:",batch)
+print("Roll no.:",roll)
