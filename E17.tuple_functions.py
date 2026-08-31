10dd3ca6276b836bf6e211b9bc4297f555a9eed1
Jacky-1010 <priyanshumishra2143@gmail.com>
2026-08-31 23:58:40 +0530

    Add files via upload

---
+++ E17.tuple_functions.py
@@ -0,0 +1,8 @@
+#creating the tuple
+num=(110,220,330,440,550,660)
+print(num)
+#usage of some functions
+print("Length of the tuple:",len(num))
+print("Greatest of the tuple:",max(num))
+print("Smallest of the tuple:",min(num))
+print("Sum of the Elements of the tuple:",sum(num))
