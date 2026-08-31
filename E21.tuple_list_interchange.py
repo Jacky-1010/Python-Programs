ed7f548040506f460b07fe4562c96d2d5e9d0be7
Jacky-1010 <priyanshumishra2143@gmail.com>
2026-09-01 00:22:59 +0530

    Add files via upload

---
+++ E21.tuple_list_interchange.py
@@ -0,0 +1,8 @@
+#Creating tuple and list
+tup=(10,9,8,7,6)
+print("tuple:",tup)
+lis=[51,52,53,54,55]
+print("list:",lis)
+#Interchanging
+print("Tuple as list:",list(tup))
+print("List as Tuple:",tuple(lis))
